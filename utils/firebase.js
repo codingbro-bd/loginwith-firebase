872cab0132dd82dea418e29d73da018b34bec337
Coding Bro <118895681+codingbro-bd@users.noreply.github.com>
2023-03-13 15:28:55 +0600

    safety

--- utils/firebase.js
+++ utils/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBobybA_NKrRNqzyoIj2olWXofk1jjtQzs",
-  authDomain: "fir-login-fa39b.firebaseapp.com",
-  projectId: "fir-login-fa39b",
-  storageBucket: "fir-login-fa39b.appspot.com",
-  messagingSenderId: "498701180858",
-  appId: "1:498701180858:web:dbc89c04861dbacd290783",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 
 // Initialize Firebase
